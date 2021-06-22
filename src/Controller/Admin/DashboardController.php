8c40d27f38574bd9d9e6b7a60cfc6a6161c5e9e0
AYA <ayaelyouss1@gmail.com>
2021-06-22 13:14:50 +0200

    git commit

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,6 +7,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\MagWarehouse;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -27,7 +28,7 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Tableau de bord', 'fa fa-home');
-        yield MenuItem::linktoDashboard('Liste des demandes', 'fa fa-search');
+        yield MenuItem::linkToCrud('Liste des demandes', 'fa fa-search',MagWarehouse::class);
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
--- src/Controller/Admin/MagWarehouseCrudController.php
+++ src/Controller/Admin/MagWarehouseCrudController.php
@@ -3,23 +3,45 @@
 namespace App\Controller\Admin;
 
 use App\Entity\MagWarehouse;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FloatField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 class MagWarehouseCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return MagWarehouse::class;
     }
-
-    /*
+    public function configureActions(Actions $actions):Actions
+    {
+        return $actions
+        ->update(Crud::PAGE_INDEX, Action::NEW,
+        fn (Action $action) =>  $action->setIcon('fa fa-plus')->addCssClass('btn btn-success'))
+        ->add(Crud::PAGE_INDEX, Action::DETAIL)
+        ->update(Crud::PAGE_INDEX, Action::EDIT,
+        fn (Action $action) =>  $action->setIcon('fa fa-edit')->addCssClass('btn btn-warning'))
+        ->update(Crud::PAGE_INDEX, Action::DETAIL,
+        fn (Action $action) =>  $action->setIcon('fa fa-eye')->addCssClass('btn btn-info'))
+        ->update(Crud::PAGE_INDEX, Action::DELETE,
+        fn (Action $action) =>  $action->setIcon('fa fa-trash')->addCssClass('btn btn-outline-danger'));
+    }
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('SkuD','Sku de la demande'),
+            Field::new('qte','Quantité'),
+            DateTimeField::new('DateE','Date d\'envoi'),
+            DateTimeField::new('DateR','Date de reception'),
+            TextField::new('statut'),
         ];
     }
-    */
+
 }
